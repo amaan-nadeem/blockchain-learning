ffa58d18bdee2a405b790e149a0b02d73f4ce6ce
amaan-nadeem <amaan.nadeem@bigdropinc.com>
2022-03-10 15:59:54 +0500

    feat: added isValidChain logic

--- crypto-chain/blockchain.js
+++ crypto-chain/blockchain.js
@@ -1,4 +1,5 @@
 const Block = require("./block");
+const cryptoHash = require("./crypto-hash");
 
 class Blockchain {
   constructor() {
@@ -12,6 +13,30 @@ class Blockchain {
     });
 
     this.chain.push(newBlock);
+  }
+
+  isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
+      return false;
+
+    for (let i = 1; i < chain.length; i++) {
+      const { timestamp, hash, lastHash, data } = chain[i];
+      const lastBlock = chain[i - 1];
+      if (lastBlock.hash !== lastHash) return false;
+      const validatedHash = cryptoHash(timestamp, data, lastHash);
+      if (hash !== validatedHash) return false;
+    }
+
+    return true;
+  }
+
+  replaceChain(chain) {
+    if (chain.length <= this.chain.length || !this.isValidChain(chain)) {
+      console.error("The incoming chain must be valid and longer");
+    } else {
+      console.log("Replacing blockchain with incoming chain", chain);
+      this.chain = chain;
+    }
   }
 }
 
